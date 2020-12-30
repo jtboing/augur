48a5db5a90e0f317fa74c5d537ab749440a0e64b
John Huddleston <huddlej@gmail.com>
2020-12-30 12:30:56 -0800

    Add tests for `write_sequences` interface
    Reorganizes the sequence-generator fixtures to be a little more flexible
    and adds tests for writing sequences in various compression modes.

--- tests/test_io.py
+++ tests/test_io.py
@@ -12,18 +12,29 @@ import sys
 
 # make sure we get the local version of modules
 sys.path.insert(0, (str(Path(__file__).parent.parent.parent)))
-from augur.io import read_sequences
+from augur.io import read_sequences, write_sequences
 
+
+def random_seq(k):
+    """Generate a single random sequence of nucleotides of length k.
+    """
+    return "".join(random.choices(("A","T","G","C"), k=k))
+
+def generate_sequences(n, k=10):
+    """Generate n random sequences of length k.
+    """
+    return (
+        SeqRecord(Seq(random_seq(k)), id=f"SEQ_{i}")
+        for i in range(1, n + 1)
+    )
 
 @pytest.fixture
 def sequences():
-    def random_seq(k):
-        return "".join(random.choices(("A","T","G","C"), k=k))
-    return [
-        SeqRecord(Seq(random_seq(10)), id="SEQ_1"),
-        SeqRecord(Seq(random_seq(10)), id="SEQ_2"),
-        SeqRecord(Seq(random_seq(10)), id="SEQ_3")
-    ]
+    return list(generate_sequences(3))
+
+@pytest.fixture
+def sequences_generator():
+    return generate_sequences(3)
 
 @pytest.fixture
 def fasta_filename(tmpdir, sequences):
@@ -105,3 +116,55 @@ class TestReadSequences:
     def test_read_sequences_from_multiple_files_with_different_compression(self, fasta_filename, gzip_fasta_filename, lzma_fasta_filename):
         sequences = read_sequences([fasta_filename, gzip_fasta_filename, lzma_fasta_filename], "fasta")
         assert len([sequence for sequence in sequences]) == 9
+
+
+class TestWriteSequences:
+    def test_write_sequences(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta")
+        sequences_written = write_sequences(sequences, output_filename, "fasta")
+        assert sequences_written == len(sequences)
+
+    def test_write_sequences_from_generator(self, tmpdir, sequences_generator):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta")
+        sequences_written = write_sequences(sequences_generator, output_filename, "fasta")
+        assert sequences_written == 3
+
+    def test_write_single_set_of_sequences_to_gzip_file(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta.gz")
+        sequences_written = write_sequences(sequences, output_filename, "fasta")
+        assert sequences_written == len(sequences)
+
+        with gzip.open(output_filename, "rt") as handle:
+            assert sequences_written == len([line for line in handle if line.startswith(">")])
+
+    def test_write_single_set_of_sequences_to_bzip2_file(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta.bz2")
+        sequences_written = write_sequences(sequences, output_filename, "fasta")
+        assert sequences_written == len(sequences)
+
+        with bz2.open(output_filename, "rt") as handle:
+            assert sequences_written == len([line for line in handle if line.startswith(">")])
+
+    def test_write_single_set_of_sequences_to_lzma_file(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta.xz")
+        sequences_written = write_sequences(sequences, output_filename, "fasta")
+        assert sequences_written == len(sequences)
+
+        with lzma.open(output_filename, "rt") as handle:
+            assert sequences_written == len([line for line in handle if line.startswith(">")])
+
+    def test_write_single_sequence_object_to_file(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta")
+        sequences_written = write_sequences(sequences[0], output_filename, "fasta")
+        assert sequences_written == 1
+
+    @pytest.mark.xfail(reason="write interface only supports a single write")
+    def test_write_single_sequence_objects_to_file_in_loop(self, tmpdir, sequences):
+        output_filename = Path(tmpdir) / Path("new_sequences.fasta")
+
+        sequences_written = 0
+        for sequence in sequences:
+            sequences_written += write_sequences(sequence, output_filename, "fasta")
+
+        with open(output_filename, 'r') as handle:
+            assert sequences_written == len([line for line in handle if line.startswith(">")])
