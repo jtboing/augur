33e5d9e4298e3b139c285d46f9d634a60340ff46
John Huddleston <huddlej@gmail.com>
2020-12-30 15:28:33 -0800

    Refactor parse logic and use read/write interface
    Refactors logic from an original for loop into its own `parse_sequence`
    function, adds tests for this new function, and updates the body of the
    `run` function to use this function inside the for loop. This commit
    also replaces the Bio.SeqIO read and write functions with the new
    `read_sequences` and `write_sequences` functions. These functions
    support compressed input and output files based on the filename
    extensions.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,3 +1,5 @@
+import Bio.SeqIO
+import Bio.Seq
 import pytest
 from pathlib import Path
 import unittest
@@ -51,3 +53,21 @@ class TestParse:
         etal_strip_string = "nextstrain et al. et al Et Al."
         assert parse.prettify(etal_lower_string, etal='lower') == etal_lower_string.lower() 
         assert parse.prettify(etal_strip_string, etal='strip') == "nextstrain   "
+
+    def test_parse_sequence(self):
+        sequence_record = Bio.SeqRecord.SeqRecord(
+            seq=Bio.Seq.Seq("ATCG"),
+            id="sequence_A",
+            description="sequence_A|USA|2020-10-03|north_america"
+        )
+        fields = ["strain", "country", "date", "region"]
+        sequence, metadata = parse.parse_sequence(
+            sequence_record,
+            fields=fields,
+            strain_key="strain",
+            prettify_fields=["region"]
+        )
+
+        assert sequence.id == "sequence_A"
+        assert set(fields) == set(metadata.keys())
+        assert metadata["region"] == "North America"
